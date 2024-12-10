16f2ebfe4cba54558ed576fc603b23a1cf08c6e9
Ronye Vernaes <remnahush@gmail.com>
2024-12-10 23:09:18 +0100

    add icon and error to input text

--- src/components/ui/InputText/index.tsx
+++ src/components/ui/InputText/index.tsx
@@ -5,6 +5,8 @@ import { FC, ChangeEvent, InputHTMLAttributes, ReactNode } from "react";
 type Props = {
   label?: ReactNode;
   value: string;
+  icon?: ReactNode;
+  error?: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 } & InputHTMLAttributes<HTMLInputElement>;
 
@@ -12,18 +14,27 @@ export const InputText: FC<Props> = ({
   label,
   value,
   onChange,
+  icon,
+  error,
   ...rest
 }) => {
   return (
     <div className="input-text">
       {label && (<span>{label}</span>)}
 
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-        {...rest}
-      />
+      <div className="input-container">
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          data-error={!!error}
+          {...rest}
+        />
+
+        {icon && <span className="input-icon">{icon}</span>}
+      </div>
+
+      {error && (<span className="input-error">{error}</span>)}
     </div>
   );
 };
