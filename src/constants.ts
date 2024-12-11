d182d9e81aa01918c63755b846f8fea53c1b6b11
Ronye Vernaes <remnahush@gmail.com>
2024-12-11 22:20:01 +0100

    extract graphql server url to a constant

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const GRAPHQL_SERVER_URL = "https://blue-api.morpho.org/graphql";
+
--- src/lib/ApolloClient/index.ts
+++ src/lib/ApolloClient/index.ts
@@ -1,7 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+import { GRAPHQL_SERVER_URL } from "../../constants";
+
 const client = new ApolloClient({
-  uri: "https://blue-api.morpho.org/graphql",
+  uri: GRAPHQL_SERVER_URL,
   cache: new InMemoryCache()
 });
 
